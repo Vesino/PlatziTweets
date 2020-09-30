685443488da71e9e6d3015f91aaa38eefecbfd9d
Luis Vargas <luisvargasr10@gmail.com>
2020-09-30 12:07:01 -0500

    Initial Commit

---
+++ PlatziTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziTweets
+//
+//  Created by Luis Vargas on 9/30/20.
+//  Copyright © 2020 Luis Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
